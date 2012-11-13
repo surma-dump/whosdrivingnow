29918a13217d7f70b6aae9d49aed66ce1b2e6e30
Alexander Surma <surma@surmair.de>
2012-11-13 11:38:32 +0100

    Implement index creation

--- process_data.go
+++ process_data.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"github.com/voxelbrain/goptions"
 	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
 	"log"
 	"net/url"
 	"os"
@@ -15,10 +16,11 @@ import (
 
 var (
 	options = struct {
-		RawFolder string        `goptions:"-r, --raw, obligatory, description='Path to the folder containing the raws'"`
-		Dry       bool          `goptions:"-n, --dry-run, description='Dont actually work on the database'"`
-		MongoURL  *url.URL      `goptions:"-m, --mongodb, description='URL pointing to MongoDB'"`
-		Help      goptions.Help `goptions:"-h, --help, description='Show this help'"`
+		RawFolder   string        `goptions:"-r, --raw, obligatory, description='Path to the folder containing the raws'"`
+		SkipImport  bool          `goptions:"--skip-import, description='Skip importing'"`
+		SkipIndexes bool          `goptions:"--skip-indexes, description='Skip creating indexes'"`
+		MongoURL    *url.URL      `goptions:"-m, --mongodb, description='URL pointing to MongoDB'"`
+		Help        goptions.Help `goptions:"-h, --help, description='Show this help'"`
 	}{
 		MongoURL: MustURL(url.Parse("mongodb://localhost")),
 	}
@@ -29,16 +31,61 @@ func init() {
 }
 
 func main() {
-	var collection *mgo.Collection
-	if !options.Dry {
-		session, err := mgo.Dial(options.MongoURL.String())
-		if err != nil {
-			log.Fatalf("Could not connect to %s: %s", options.MongoURL.String(), err)
-		}
-		defer session.Close()
-		collection = session.DB("").C("raw")
+	session, err := mgo.Dial(options.MongoURL.String())
+	if err != nil {
+		log.Fatalf("Could not connect to %s: %s", options.MongoURL.String(), err)
 	}
-	filepath.Walk(options.RawFolder, func(path string, fi os.FileInfo, err error) error {
+	defer session.Close()
+	collection := session.DB("").C("raw")
+	if !options.SkipImport {
+		c := readFiles(options.RawFolder)
+		for v := range c {
+			err := collection.Insert(v)
+			if err != nil {
+				log.Printf("Inserting of %s (%s) failed: %s", v.Name, v.Timestamp, err)
+				continue
+			}
+		}
+	}
+
+	if !options.SkipIndexes {
+		log.Printf("Creating indexes...")
+		collection.EnsureIndex(mgo.Index{
+			Key: []string{"timestamp"},
+		})
+		collection.EnsureIndex(mgo.Index{
+			Key: []string{"name"},
+		})
+	}
+
+	names := []string{}
+	err = collection.Find(nil).Distinct("name", &names)
+	if err != nil {
+		log.Fatalf("Could not query car names: %s", err)
+	}
+	for _, name := range names {
+		log.Printf("Extracting routes of %s...", name)
+		iter := collection.Find(bson.M{
+			"name": name,
+		}).Sort("timestamp").Iter()
+		var cur, last drivenow.Vehicle
+		for iter.Next(&cur) {
+			log.Printf("cur = %#v, last = %#v", cur, last)
+			last = cur
+		}
+	}
+}
+
+func MustURL(u *url.URL, err error) *url.URL {
+	if err != nil {
+		panic(err)
+	}
+	return u
+}
+
+func readFiles(rawfolder string) chan *drivenow.Vehicle {
+	c := make(chan *drivenow.Vehicle)
+	go filepath.Walk(options.RawFolder, func(path string, fi os.FileInfo, err error) error {
 		if fi.IsDir() {
 			return nil
 		}
@@ -69,30 +116,19 @@ func main() {
 				} `json:"vehicles"`
 			} `json:"rec"`
 		}{}
+		log.Printf("Decoding %s...", filename)
 		dec := json.NewDecoder(f)
 		err = dec.Decode(&container)
 		if err != nil {
 			log.Printf("Invalid content in %s: %s", path, err)
 			return nil
 		}
-		log.Printf("Adding %4d entries of %s", len(container.Rec.Vehicles.Vehicles), timestamp)
-		for i, v := range container.Rec.Vehicles.Vehicles {
+		for _, v := range container.Rec.Vehicles.Vehicles {
 			v.Timestamp = timestamp
-			if !options.Dry {
-				err := collection.Insert(v)
-				if err != nil {
-					log.Printf("Inserting %4d failed: %s", i, err)
-					continue
-				}
-			}
+			c <- &v
 		}
+		close(c)
 		return nil
 	})
-}
-
-func MustURL(u *url.URL, err error) *url.URL {
-	if err != nil {
-		panic(err)
-	}
-	return u
+	return c
 }
